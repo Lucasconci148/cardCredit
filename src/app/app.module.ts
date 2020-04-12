ae4639ea869e000eb824a153e6b52212927a6978
e_lconci <lucas.conci@naranja.com>
2020-04-12 10:24:01 -0300

    add img and form whit angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from "@angular/forms"
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -8,8 +8,42 @@ import { Component } from '@angular/core';
 
 export class CardComponent {
     public isBack = false;
+    public name = '';
+    public cvv  = '';
+    public number = '';
+    public isVisa = false;
+    public isMaster = false;
+
+    public month = '';
+    public year = '';
 
     clickEvent() {
         this.isBack = !this.isBack;
+    }
+
+    shoBack() {
+      this.isBack = true;
+    }
+
+    showFront() {
+      this.isBack = false;
+    }
+
+    changeCardNumber() {
+      console.log(this.number.charAt(0));
+      switch (this.number.charAt(0)) {
+        case '5':
+          this.isMaster = true;
+          this.isVisa = false;
+          break;
+        case '4':
+          this.isMaster = false;
+          this.isVisa = true;
+          break;
+        default:
+          this.isMaster = false;
+          this.isVisa = false
+          break;
+      }
     }
 }
